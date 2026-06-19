fcd8ab691e9eaf77feacf4c7e308937628ebebb4
pruthviraj1111dream <pruthvirajc253@gmail.com>
2026-06-19 22:27:35 +0530

    Update db.js

--- dental111/db.js
+++ dental111/db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-host:'localhost',
-user:'root',
-password:'root',
-database: 'dental'
+host:'bbgt8qrmbbf72p1b3kye-mysql.services.clever-cloud.com',
+user:'uqsyrigzjkamvhq6',
+password:'4c0W3solA8Vk25xAcSxj',
+database: 'bbgt8qrmbbf72p1b3kye'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
